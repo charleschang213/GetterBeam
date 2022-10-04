72d76445d5e5d59f7950b62901be46047635f5de
Yichi Zhang <yichiz213@gmail.com>
2022-10-04 21:14:06 +0800

    feat(init): init project files

---
+++ src/GetterBeam.cpp
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * @file     GetterBeam.cpp
+ * @brief    Getter Beam CPU Interface
+ * @author   Yichi Zhang
+ * @date     2022/10/4
+ *****************************************************************************/
+#include "../include/GetterBeam.h"
+
+namespace GetterBeam
+{
+std::string GBCPU::HelloWorldText()
+{
+	return "Hello World";
+}
+}	 // namespace GetterBeam
